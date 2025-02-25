ad67329d91daf03cbba3d8fcea89abf9848fc7e1
tarun-jaat <badgujjar9991@gmail.com>
2025-02-25 22:17:36 +0530

    this is the last cakk

--- server.js
+++ server.js
@@ -29,5 +29,14 @@ connectWithRetry();
 app.use('/api/schedule-meeting', meetingRoutes);
 app.use('/api/users', userRoutes);
 
+
+
+
+
 const PORT = process.env.PORT || 5001;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
